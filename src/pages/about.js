a5666c88d364fd23151ed71db9e2ca5208b25bde
Pratima <pratima@new-host-2.local>
2019-05-10 17:32:43 +0200

    about page added

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { FaGulp } from "react-icons/fa"
+import { Link, graphql } from "gatsby"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="About US"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+export default AboutPage
